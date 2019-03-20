c2b6b7a0f201cd1e5fe724f8fefd7b2bec9566ff
matthewpoletin <poletinm@yandex.ru>
2019-03-20 11:44:10 +0300

    Unity initial project

---
+++ Assets/Client/Scripts/View/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Client.Scripts.View
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraController : MonoBehaviour
+    {
+        private Camera _camera;
+
+        private void Start()
+        {
+            _camera = GetComponent<Camera>();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ambidex-view
 Visualization tools for Ambidex
+
+## Required Unity Packages
+- ProBuilder 4.0.3
+- ProGrids 3.0.3
